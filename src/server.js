c9107998f872021db521f4ef865723c618d23528
iCarambaaa <dkpress1916@zoho.eu>
2021-11-12 11:29:28 +0100

    init

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import listEndpoints from 'express-list-endpoints';
+import mongoose from "mongoose"
+//import blogsRouter from "./services/blogs/index.js";
+//import authorsRouter from "./services/authors/index.js"
+import { notFoundHandler, badRequestHandler, genericErrorHandler, unauthorizedHandler } from "./errorHandlers.js"
+const server = express();
+
+const port = process.env.PORT || 3001
+
+// ********************************* MIDDLEWARES ***************************************
+
+server.use(cors())
+server.use(express.json())
+
+// ********************************* ROUTES ********************************************
+
+//server.use("/blogs", blogsRouter)
+//server.use("/authors", authorsRouter)
+
+// ********************************* ERROR HANDLERS ************************************
+
+server.use(notFoundHandler)
+server.use(unauthorizedHandler)
+server.use(badRequestHandler)
+server.use(genericErrorHandler)
+
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+mongoose.connection.on("connected", () => {
+    console.log("Mongo Connected!")
+
+
+server.listen(port, () => {
+    
+  console.table(listEndpoints(server))
+    console.log(`✅ Server running on port ${port}`)
+})
+})
+
+mongoose.connection.on("error", err => {
+  console.log(err)
+})
