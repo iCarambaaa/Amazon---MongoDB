99ae6248943543a2358e50c2b45296278a4461c3
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-11-12 15:52:29 +0100

    final

--- src/services/User/index.js
+++ src/services/User/index.js
@@ -15,13 +15,16 @@ const {
 } = userendpoint;
 
 const userRouter = express.Router();
+
 userRouter.route('/').post(createUser).get(getAllUser);
+
 userRouter.route('/:id').put(updateUser).get(getUserById).delete(deleteUser);
 
 userRouter
 	.route('/:id/purchaseHistory')
 	.post(pushproductinCart)
 	.get(getproductsinCart);
+
 userRouter
 	.route('/:id/purchaseHistory/:productId')
 	.get(getoneproductsinCart)
