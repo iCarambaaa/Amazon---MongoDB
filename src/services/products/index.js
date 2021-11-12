e5e00099d6a0367d76d23fb7d756fa0f0636ac41
iCarambaaa <dkpress1916@zoho.eu>
2021-11-12 15:13:35 +0100

    cloudinary

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,7 +1,18 @@
 import express from "express"
 import productHandlers from "./handlers.js"
+import {CloudinaryStorage} from "multer-storage-cloudinary";
+import multer from "multer";
+import {v2 as cloudinary} from "cloudinary";
+
+
+const cloudinaryStorage = new CloudinaryStorage({cloudinary, params: {folder: "amazonBE"}})
 
 const productsRouter = express.Router()
+
+
+productsRouter.route("/image")
+.post(multer({storage: cloudinaryStorage}).single("img"), productHandlers.addImageToProduct)
+
 
 productsRouter.route("/")
 .get(productHandlers.getAllProducts)
@@ -11,5 +22,7 @@ productsRouter.route("/:id")
 .get(productHandlers.getSingleProduct)
 .put(productHandlers.editSingleProduct)
 .delete(productHandlers.deleteSingleProduct)
+
+
 
 export default productsRouter
